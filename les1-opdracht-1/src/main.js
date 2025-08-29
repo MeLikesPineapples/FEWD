acd9585cf8f2a7d30cd4d943751cbc7ecc4caab3
MeLikesPineapples <qa.tjonsoeilen@student.han.nl>
2025-08-29 22:12:46 +0200

    Huisdieren toegevoegd volgens de opdracht.

--- les1-opdracht-1/src/main.js
+++ les1-opdracht-1/src/main.js
@@ -3,6 +3,7 @@ document.title = 'My Magical Home Page';
 const louisa = {
   naam: 'Louisa',
   leeftijd: 19,
+  huisdieren: ['toekan', 'komodovaraan', 'capybara'],
   
   getBirthDate: function () {
     const currentYear = new Date().getFullYear();
@@ -13,6 +14,13 @@ const louisa = {
     console.log('Hallo ik ben ' + this.naam, 'en ik ben ' + this.leeftijd + ' jaar oud. Ik ben geboren in ' + this.getBirthDate() + '.');
   },
   
+  showPets: function () {
+    console.log('Ik heb de volgende huisdieren:');
+    for (let i = 0; i < this.huisdieren.length; i++) {
+      console.log(this.huisdieren[i] + '.');
+    }
+  }
+
 };
 
 louisa.talk();
